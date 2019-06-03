b8cd5f0f9d87f4b946fb692884329e4ea9f2e5cc
stefian <v_ianta@yahoo.com>
2019-06-03 08:08:00 -0400

    initial commit

---
+++ src/UI/Header.svelte
@@ -0,0 +1,35 @@
+<style>
+  header {
+    position: fixed;
+    width: 100%;
+    top: 0;
+    left: 0;
+    height: 4rem;
+    /* background: #cf0056; */
+    background: #6aa84f;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    box-shadow: 0 2px 6px rgba(0, 0, 0, 0.26);
+  }
+
+  h1 {
+      /* font-size: 1.1rem; */
+    color: white;
+    font-family: "Roboto Slab", serif;
+    margin: 0;
+  }
+
+  p {
+    font-size: 0.59rem;
+    color: white;
+    font-family: "Roboto Slab", sans-serif;
+    margin: 0;
+  }
+</style>
+
+<header>
+  <h1>innovation teams</h1>
+  <p>join 10 million innovators on your birthday! or any year day!</p>
+</header>
